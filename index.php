c2b64bbfb55614d11b4f5210c1623403cb71667c
Geraxi <umbertogeraci0@gmail.com>
2025-02-11 10:12:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,75 +1,35 @@
 <?php
 
-abstract class Ironman {
-    protected static $count = 0;
-    protected $model;
+require('laser.php');
+require('repulsor.php');
+require('flight.php');
+require('shield.php');
 
-    public function __construct($model) {
-        $this->model = $model;
-        self::$count++;
+class IronmanArmor {
+    public $attack;
+    public $defense;
+    public static $counter = 0;
+
+  
+    public function __construct(Attack $attacco, Defense $difesa) {
+        $this->attack = $attacco;  
+        $this->defense = $difesa;   
+        self::$counter++;
     }
 
-    abstract public function getModel();
-
-    public static function getIronmanCount() {
-        return self::$count;
+    public function ironmanAttack() {
+        $this->attack->attack();
     }
-}
-
-class Mark1 extends Ironman {
-    public function __construct() {
-        parent::__construct("Mark 1");
+    public function ironmanDefense() {
+        $this->defense->defend();
     }
+}  
 
-    public function getModel() {
-        return "Questo è il modello: " . $this->model;
-    }
-}
 
-class Mark2 extends Ironman {
-    public function __construct() {
-        parent::__construct("Mark 2");
-    }
+$ironman = new IronmanArmor(new Laser(),new EnergyShield());  
+$ironman->ironmanAttack();
+$ironman->ironmanDefense();
+echo IronmanArmor::$counter . "\n"; 
 
-    public function getModel() {
-        return "Questo è il modello: " . $this->model;
-    }
-}
-
-class Mark3 extends Ironman {
-    public function __construct() {
-        parent::__construct("Mark 3");
-    }
-
-    public function getModel() {
-        return "Questo è il modello: " . $this->model;
-    }
-}
-
-class Army {
-    private $ironmen = [];
-
-    public function __construct($count) {
-        for ($i = 0; $i < $count; $i++) {
-            $this->ironmen[] = $this->createRandomIronman();
-        }
-    }
-
-    private function createRandomIronman() {
-        $models = [new Mark1(), new Mark2(), new Mark3()];
-        return $models[array_rand($models)];
-    }
-
-    public function showArmy() {
-        foreach ($this->ironmen as $ironman) {
-            echo $ironman->getModel() . "<br>";
-        }
-        echo "Totale Ironman creati: " . Ironman::getIronmanCount() . "<br>";
-    }
-}
-
-// Creiamo un esercito di 10 Ironman
-$ironmanArmy = new Army(10);
-$ironmanArmy->showArmy();
-
-?>
+$ironman2 = new IronmanArmor(new Repulsor(), new Flight());
+echo IronmanArmor::$counter . "\n";
